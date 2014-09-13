87a0a0b87638d16c9ecc04e74ec5224aa18b71e8
James Tomasino <james@tomasino.org>
2014-09-13 11:07:49 -0400

    much better images for swapping modes

--- video/feed.js
+++ video/feed.js
@@ -64,3 +64,35 @@ function checkScroll () {
 	}
 }
 
+var bDayYear = 2012;
+var bDayMonth = 8; // August
+var bDayDate = 19;
+
+$(function() {
+	// Show age
+	var witBDay = new Date ( bDayYear, bDayMonth - 1, bDayDate );
+	var today = new Date ();
+	var age = today - witBDay;
+	var weeks = Math.floor(age / 604800000);
+	var years = Math.floor(age / 31536000000);
+
+	// Compensate for the birthday on the 19th and calculate months
+	var wM = new Date( witBDay - (1000 * 60 * 60 * 24 * (bDayDate - 1)) );
+	var tM = new Date( today - (1000 * 60 * 60 * 24 * (bDayDate - 1)) );
+	var months = tM.getMonth() - wM.getMonth() + (12 * ( tM.getFullYear() - wM.getFullYear()));
+
+	var ageHTML = $('<div class="age"></div>');
+	var weeksHTML = $('<span class="label">Age:</span><span class="value">' + weeks + '</span><span class="units">weeks</span>');
+	var monthsHTML = $('<span class="label">Age:</span><span class="value">' + months + '</span><span class="units">months</span>');
+	var yearsHTML = $('<span class="label">Age:</span><span class="value">' + years + '</span><span class="units">years</span>');
+
+	if (weeks < 52)
+		ageHTML.append(weeksHTML)
+	else if ( months < 24 )
+		ageHTML.append(monthsHTML)
+	else
+		ageHTML.append(yearsHTML);
+
+	$('#info').html(ageHTML);
+
+});
